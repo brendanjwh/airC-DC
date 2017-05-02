1907df84a91ff26d43eec96ddcec27e96b2465c0
Alex Origitano <aorigitano@gmail.com>
2017-05-02 16:17:41 -0500

    bass sound is working, dk why an error of buttons not defined is not working

--- public/javascripts/bass.js
+++ public/javascripts/bass.js
@@ -4,18 +4,18 @@
 var buttons = [];
 
 var button1 = new Image();
-button1.src ="assets/images/cowbell.jpeg";
+button1.src ="images/cowbell.jpeg";
 var buttonData1 = { name:"strum", image:button1, x:80, y:305, w:70, h:70, class:"strum" };
 buttons.push( buttonData1 );
 
 
 var button2 = new Image();
-button2.src ="assets/images/cowbell.jpeg";
+button2.src ="images/cowbell.jpeg";
 var buttonData2 = { name:"E", image:button2, x:339, y:100, w:30, h:30 };
 buttons.push( buttonData2 );
 
 var button3 = new Image();
-button3.src ="assets/images/cowbell.jpeg";
+button3.src ="images/cowbell.jpeg";
 var buttonData3 = { name:"G", image:button2, x:270, y:150, w:30, h:30 };
 buttons.push( buttonData3 );
 
