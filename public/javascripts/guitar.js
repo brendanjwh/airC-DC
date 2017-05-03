3edcc322501852789f04b321dc60ea7c2b0104e4
Brendan White <SKYNET@Brendans-MacBook.local>
2017-05-03 13:58:03 -0500

    guitar is working

--- public/javascripts/guitar.js
+++ public/javascripts/guitar.js
@@ -4,34 +4,34 @@
 var buttons = [];
 
 var button1 = new Image();
-button1.src ="public/images/cowbell.png";
+button1.src ="/images/cowbell.png";
 var buttonData1 = { name:"strum", image:button1, x:50, y:700, w:160, h:160, class:"strum" };
 buttons.push( buttonData1 );
 
 
 var button2 = new Image();
-button2.src ="public/images/cowbell.png";
+button2.src ="/images/cowbell.png";
 var buttonData2 = { name:"G", image:button2, x:1180, y:30, w:80, h:80 };
 buttons.push( buttonData2 );
 
 var button3 = new Image();
-button3.src ="public/images/cowbell.png";
-var buttonData3 = { name:"D", image:button2, x:1000, y:160, w:80, h:80};
+button3.src ="/images/cowbell.png";
+var buttonData3 = { name:"A", image:button2, x:1000, y:160, w:80, h:80};
 buttons.push( buttonData3 );
 
 var button4 = new Image();
-button4.src ="public/images/cowbell.png";
-var buttonData4 = { name:"strum", image:button4, x:800, y:300, w:80, h:80, class:"strum" };
+button4.src ="/images/cowbell.png";
+var buttonData4 = { name:"B", image:button4, x:800, y:300, w:80, h:80, class:"strum" };
 buttons.push( buttonData4 );
 
 var button5 = new Image();
-button5.src ="public/images/cowbell.png";
-var buttonData5 = { name:"G", image:button5, x:590, y:450, w:80, h:80 };
+button5.src ="/images/cowbell.png";
+var buttonData5 = { name:"D", image:button5, x:590, y:450, w:80, h:80 };
 buttons.push( buttonData5 );
 
 var button6 = new Image();
-button6.src ="public/images/cowbell.png";
-var buttonData6 = { name:"D", image:button2, x:360, y:590, w:80, h:80 };
+button6.src ="/images/cowbell.png";
+var buttonData6 = { name:"E_hi", image:button2, x:360, y:590, w:80, h:80 };
 buttons.push( buttonData6 );
 
 notes = ["A", "B", "D", "strum", "E_hi", "G"];
