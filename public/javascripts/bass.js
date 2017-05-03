f100536993839e30073e2b2e1330b4593b15885c
Alex Origitano <aorigitano@gmail.com>
2017-05-03 18:57:33 -0500

    add notes for images instead of cowbell for bass

--- public/javascripts/bass.js
+++ public/javascripts/bass.js
@@ -1,35 +1,40 @@
 /* ======================================================== 
               javascript sound button set up
    ======================================================== */
+
+// var button7 = new Image();
+// button7.src ="/images/bass.png";
+// var buttonData7 = { name:"guitar", image:button7, x:100, y:750, w:1060, h:-500 };
+
 var buttons = [];
 
 var button1 = new Image();
-button1.src ="images/cowbell.png";
+button1.src ="images/pick.png";
 var buttonData1 = { name:"strum", image:button1, x:50, y:700, w:160, h:160, class:"strum" };
 buttons.push( buttonData1 );
 
 var button2 = new Image();
-button2.src ="images/cowbell.png";
+button2.src ="images/g.png";
 var buttonData2 = { name:"G", image:button2, x:1180, y:30, w:100, h:100 };
 buttons.push( buttonData2 );
 
 var button3 = new Image();
-button3.src ="images/cowbell.png";
+button3.src ="images/a.png";
 var buttonData3 = { name:"A", image:button2, x:1000, y:160, w:100, h:100 };
 buttons.push( buttonData3 );
 
 var button4 = new Image();
-button4.src ="images/cowbell.png";
+button4.src ="images/b.png";
 var buttonData4 = { name:"B", image:button2, x:800, y:300, w:100, h:100 };
 buttons.push( buttonData4 );
 
 var button5 = new Image();
-button5.src ="images/cowbell.png";
+button5.src ="images/d.png";
 var buttonData5 = { name:"D", image:button2, x:590, y:450, w:100, h:100 };
 buttons.push( buttonData5 );
 
 var button6 = new Image();
-button6.src ="images/cowbell.png";
+button6.src ="images/e.png";
 var buttonData6 = { name:"E_hi", image:button2, x:360, y:590, w:100, h:100 };
 buttons.push( buttonData6 );
 
