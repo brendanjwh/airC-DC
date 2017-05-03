494f5be4616155af82940ea7780386707377719f
Brendan White <SKYNET@Brendans-MacBook.local>
2017-05-03 12:02:41 -0500

    bass done

--- public/javascripts/bass.js
+++ public/javascripts/bass.js
@@ -5,33 +5,33 @@ var buttons = [];
 
 var button1 = new Image();
 button1.src ="images/cowbell.jpeg";
-var buttonData1 = { name:"strum", image:button1, x:0, y:350, w:90, h:90, class:"strum" };
+var buttonData1 = { name:"strum", image:button1, x:50, y:700, w:160, h:160, class:"strum" };
 buttons.push( buttonData1 );
 
 
 var button2 = new Image();
 button2.src ="images/cowbell.jpeg";
-var buttonData2 = { name:"G", image:button2, x:460, y:0, w:50, h:40 };
+var buttonData2 = { name:"G", image:button2, x:1180, y:30, w:80, h:80 };
 buttons.push( buttonData2 );
 
 var button3 = new Image();
 button3.src ="images/cowbell.jpeg";
-var buttonData3 = { name:"A", image:button2, x:370, y:70, w:45, h:45 };
+var buttonData3 = { name:"A", image:button2, x:1000, y:160, w:80, h:80 };
 buttons.push( buttonData3 );
 
 var button4 = new Image();
 button4.src ="images/cowbell.jpeg";
-var buttonData4 = { name:"B", image:button2, x:290, y:130, w:45, h:45 };
+var buttonData4 = { name:"B", image:button2, x:800, y:300, w:80, h:80 };
 buttons.push( buttonData4 );
 
 var button5 = new Image();
 button5.src ="images/cowbell.jpeg";
-var buttonData5 = { name:"D", image:button2, x:210, y:190, w:45, h:45 };
+var buttonData5 = { name:"D", image:button2, x:590, y:450, w:80, h:80 };
 buttons.push( buttonData5 );
 
 var button6 = new Image();
 button6.src ="images/cowbell.jpeg";
-var buttonData6 = { name:"E_hi", image:button2, x:120, y:255, w:45, h:45 };
+var buttonData6 = { name:"E_hi", image:button2, x:360, y:590, w:80, h:80 };
 buttons.push( buttonData6 );
 
 notes = ["A", "B", "D", "strum", "E_hi", "G"];
