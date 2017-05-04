b128297cccb030cebd9835517deaa1b126aaed5c
Alex Origitano <aorigitano@gmail.com>
2017-05-03 19:13:07 -0500

    add so bass guitar shows up

--- public/javascripts/bass_app.js
+++ public/javascripts/bass_app.js
@@ -90,6 +90,7 @@ function render()
   {
     // mirror video
     videoContext.drawImage( video, 0, 0, videoCanvas.width, videoCanvas.height );
+     layer2Context.drawImage( buttonData7.image, buttonData7.x, buttonData7.y, buttonData7.w, buttonData7.h );
     for ( var i = 0; i < buttons.length; i++ ) {
       //this is where the buttons are being placed on one canvas.
       layer2Context.drawImage( buttons[i].image, buttons[i].x, buttons[i].y, buttons[i].w, buttons[i].h );        
